a0588817e7b68f5ea906d8ba22fc0946647b342f
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-13 14:53:04 +0300

    commit tree 7c9a99a4dd7edc3c31a93f888b692bcdf6d203dc

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    let paragraph = document.querySelector("p#text")
+    paragraph.textContent = "This is really cool!"
+})
